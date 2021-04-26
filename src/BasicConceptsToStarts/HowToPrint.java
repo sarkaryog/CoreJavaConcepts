f19e5427cd928de9c4c3ce55295c673d0552ecb0
Yogesh <sarkar.yog@gmail.com>
2021-04-26 17:08:11 +0530

    First commit

---
+++ src/BasicConceptsToStarts/HowToPrint.java
@@ -0,0 +1,14 @@
+package BasicConceptsToStarts;
+
+public class HowToPrint {
+
+	// Execution should start from the main method
+	// Java is case sensitive
+	
+	public static void main(String[] args) {
+		
+		System.out.println("These concepts are provided by Yogesh");
+
+	}
+
+}
